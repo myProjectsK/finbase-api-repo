9c44403a8f9fa10bc4d9d0a64b070732f81f956f
Karthik Kulkarni <Karthikkulkarni0710@gmail.com>
2025-03-24 22:40:38 +0530

    Added login user model

---
+++ FinBaseWebApp/Models/LoginModel.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FinBaseWebApp.Models
+{
+    public class LoginModel
+    {
+        public string UserId { get; set; }
+        [JsonProperty("Mobile")]
+        public string RoleName { get; set; }
+        public string MobileNo { get; set; }
+        public string Name { get; set; }
+        [JsonProperty("Email")]
+        public string EmailId { get; set; }
+        public string PasswordHash { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public String ModifiedBy { get; set; }
+        public DateTime ModifiedAt { get; set; }
+        public bool Status { get; set; }
+    }
+}
