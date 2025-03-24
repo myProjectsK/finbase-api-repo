81b1cff5ecd864cc7f992aff422aef577ea80536
Karthik Kulkarni <Karthikkulkarni0710@gmail.com>
2025-03-24 22:36:51 +0530

    Updated

--- FinBaseWebApp/Startup.cs
+++ FinBaseWebApp/Startup.cs
@@ -1,42 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Http;
-using FinBaseWebApp.Repository;
-using Microsoft.Owin;
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.Jwt;
+using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.OAuth;
 using Owin;
+using System.Text;
+using System.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Web.Http;
+using FinBaseWebApp;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Owin.Cors;
 
-[assembly: OwinStartup(typeof(FinBaseWebApp.Startup))]
+[assembly: OwinStartup(typeof(FinBaseWebApp.Startup))] 
 
-namespace FinBaseWebApp
+public partial class Startup
 {
-    public partial class Startup
+    public void Configuration(IAppBuilder app)
     {
+        ConfigureAuth(app);     
 
-        public void ConfigurationAuth(IAppBuilder app)
-        {
-     
-            OAuthAuthorizationServerOptions options = new OAuthAuthorizationServerOptions
-            {
-                AllowInsecureHttp = true,
-                TokenEndpointPath = new PathString("/login"),
-                AccessTokenExpireTimeSpan = TimeSpan.FromMinutes(300),      
-                Provider = new AuthorizationServerProvider(),       
-                RefreshTokenProvider = new RefreshTokenProvider()
-            };
-
-            app.UseOAuthAuthorizationServer(options);       
-            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
-
-            HttpConfiguration config = new HttpConfiguration();
-            WebApiConfig.Register(config);      
-        }   
-
-        public void Configuration(IAppBuilder app)
-        {
-            ConfigurationAuth(app);     
-        }
+        HttpConfiguration config = new HttpConfiguration();
+        WebApiConfig.Register(config);
+        app.UseWebApi(config);
     }
+
+    public void ConfigureAuth(IAppBuilder app)
+    {
+        app.UseCors(CorsOptions.AllowAll);      
+        var secretKey = Encoding.ASCII.GetBytes(ConfigurationManager.AppSettings["JwtSecretKey"]); 
+
+        app.UseJwtBearerAuthentication(new JwtBearerAuthenticationOptions
+        {
+            AuthenticationMode = AuthenticationMode.Active,
+            TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(secretKey), 
+                ValidateIssuer = false, 
+                ValidateAudience = false, 
+                ValidateLifetime = true,
+                ClockSkew = TimeSpan.Zero
+            }
+        });     
+    }
+
 }
 
