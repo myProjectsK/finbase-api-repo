0665ef8659656e1536a5cd55145cd15717905d81
Karthik Kulkarni <Karthikkulkarni0710@gmail.com>
2025-04-21 20:14:37 +0530

    Added AppConfigController to serve app configuration

---
+++ FinBaseWebApp/Controllers/AppConfigController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Configuration;
+
+namespace FinBaseWebApp.Controllers
+{
+    [RoutePrefix("api/config")] 
+    public class AppConfigController : ApiController
+    {
+        [HttpGet]
+        [Route("")]
+        public IHttpActionResult GetAppConfig()
+        {
+            // Create a configuration object to return to the client
+            var config = new
+            {
+                ApiBaseUrl = ConfigurationManager.AppSettings["JwtIssuer"],
+                Features = new
+                {
+                    EnableNotifications = bool.Parse(ConfigurationManager.AppSettings["EnableNotifications"] ?? "false"),
+                    EnableDarkMode = bool.Parse(ConfigurationManager.AppSettings["EnableDarkMode"] ?? "false")
+                },
+                Timeouts = new
+                {
+                    SessionTimeout = int.Parse(ConfigurationManager.AppSettings["JwtExpiryMinutes"] ?? "1440")
+                }
+            };
+
+            return Ok(config);
+        }   
+    }   
+}   
