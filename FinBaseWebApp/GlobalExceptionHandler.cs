e8c68aa2238b4143565c79bdfbebbdd65842c209
Karthik Kulkarni <Karthikkulkarni0710@gmail.com>
2025-07-13 13:16:17 +0530

    Changes

---
+++ FinBaseWebApp/GlobalExceptionHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Filters;
+
+public class GlobalExceptionHandler : ExceptionFilterAttribute
+{
+    public override void OnException(HttpActionExecutedContext context)
+    {
+        var exception = context.Exception;
+
+        Console.WriteLine($"Exception Message: {exception.Message}");
+        Console.WriteLine($"Exception StackTrace: {exception.StackTrace}");
+
+        if (exception.InnerException != null)
+        {
+            Console.WriteLine($"Inner Exception: {exception.InnerException.Message}");
+        }
+
+        var response = context.Request.CreateResponse(HttpStatusCode.InternalServerError, new
+        {
+            Message = "Error processing request",
+            ExceptionType = exception.GetType().Name,
+            ExceptionMessage = exception.Message,   
+            InnerExceptionMessage = exception.InnerException?.Message,      
+            StackTrace = exception.StackTrace       
+        });
+
+        context.Response = response; 
+    }
+}
