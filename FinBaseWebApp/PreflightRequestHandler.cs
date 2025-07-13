f86dd9bd15571af7eb3343d3d1fedf55676a3f79
Karthik Kulkarni <Karthikkulkarni0710@gmail.com>
2025-07-13 13:16:48 +0530

    Changes

---
+++ FinBaseWebApp/PreflightRequestHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+public class PreflightRequestHandler : DelegatingHandler
+{
+    protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        if (request.Method == HttpMethod.Options)
+        {
+            var response = new HttpResponseMessage(HttpStatusCode.OK);
+            response.Headers.Add("Access-Control-Allow-Origin", "*");
+            response.Headers.Add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+            response.Headers.Add("Access-Control-Allow-Headers", "Content-Type, Authorization");
+            return Task.FromResult(response);
+        }
+        return base.SendAsync(request, cancellationToken);
+    }
+}
